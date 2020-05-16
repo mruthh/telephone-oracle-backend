633314e54e0ea7fb423676d0f05a17a28b3b4dee
Megan Honig <meganhonig@gmail.com>
2020-05-16 14:23:39 -0400

    fix require stmts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-require(dotenv).config()
+require('dotenv').config()
 
 const { resolve } = require('path')
 
--- sheet.js
+++ sheet.js
@@ -1,4 +1,4 @@
-import { Sheet } from './db/models/Sheet'
+const { Sheet } = require('./db/models/Sheet')
 
 const getSheets = (gameId) => {
   return Sheet.findAll({ where: { game_id: gameId }})
